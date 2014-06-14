7f56906abd69b086e339b8ddfd55d7c5c8af50f6
Praveen Chavan <pchavan@codeaurora.org>
2014-06-13 17:59:37 -0700

    msm: vidc: update with fixes and enhancements
    detect mbaff information from firmware
    
    Firmware will set the FBD flags if mbaff interface
    is detected for decoded video bitstream. Previously
    the mbaff information was parsed in the v4l2 video
    client, but now firmware parses and sends the information.
    
    Add generic LTR mode support
    Use generic LTR HFI API for H264 and VP8 formats.
    
    Add support to set initial qp, thereby allowing the client to set
    initial qp for I,P, and B frames.
    
    Add support for narrow search range setting which allow clients
    to set motion search range window.
    
    Change-Id: Ib47811ae2839070f71253249edfd82582eef2299
    Signed-off-by: Arun Menon <avmenon@codeaurora.org>
    Signed-off-by: Ashray Kulkarni <ashrayk@codeaurora.org>
    Signed-off-by: Srinu Gorle <sgorle@codeaurora.org>
    Signed-off-by: Sachin Shah <sachins@codeaurora.org>
    Signed-off-by: Praveen Chavan <pchavan@codeaurora.org>
    
    Conflicts:
    	drivers/media/platform/msm/vidc/hfi_packetization.c
    	drivers/media/platform/msm/vidc/vidc_hfi_api.h

--- drivers/media/platform/msm/vidc/vidc_hfi_api.h
+++ drivers/media/platform/msm/vidc/vidc_hfi_api.h
@@ -206,6 +206,7 @@ enum hal_property {
 	HAL_CONFIG_VENC_HIER_P_NUM_FRAMES,
 	HAL_PARAM_VENC_HIER_P_MAX_ENH_LAYERS,
 	HAL_PARAM_VENC_ENABLE_INITIAL_QP,
+	HAL_PARAM_VENC_SEARCH_RANGE,
 };
 
 enum hal_domain {
@@ -863,6 +864,13 @@ struct hal_vpe_color_space_conversion {
 	u32 csc_matrix[HAL_MAX_MATRIX_COEFFS];
 	u32 csc_bias[HAL_MAX_BIAS_COEFFS];
 	u32 csc_limit[HAL_MAX_LIMIT_COEFFS];
+};
+
+struct hal_vc1e_perf_cfg_type {
+	struct {
+		u32 x_subsampled;
+		u32 y_subsampled;
+	} i_frame, p_frame, b_frame;
 };
 
 enum vidc_resource_id {
