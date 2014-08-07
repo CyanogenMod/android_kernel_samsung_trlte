ae65333dd0673a772f3a9d2059e66b35ed684cb2
Saurabh Shah <saurshah@codeaurora.org>
2014-08-07 09:46:49 -0700

    msm: mdss: Remove the unused has_buf pipe member
    The has_buf was introduced for a special case where buffers may not
    be queued to MDP directly. Remove the has_buf variable since it is
    not used anymore.
    
    Change-Id: Ie8137991dc771b8b17da1f35d4bbbeb62ae8658b
    Signed-off-by: Saurabh Shah <saurshah@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_mdp_overlay.c
    	drivers/video/msm/mdss/mdss_mdp_pipe.c

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -833,7 +833,6 @@ int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 	}
 
 	pipe->params_changed++;
-	pipe->has_buf = 0;
 
 	req->vert_deci = pipe->vert_deci;
 
@@ -1689,7 +1688,6 @@ static int mdss_mdp_overlay_queue(struct msm_fb_data_type *mfd,
 	ret = mdss_mdp_data_get(src_data, &req->data, 1, flags);
 	if (IS_ERR_VALUE(ret))
 		pr_err("src_data pmem error\n");
-	pipe->has_buf = 1;
 
 	mdss_mdp_pipe_unmap(pipe);
 
@@ -1890,7 +1888,6 @@ static void mdss_mdp_overlay_pan_display(struct msm_fb_data_type *mfd)
 	buf->p[0].addr += offset;
 	buf->p[0].len = fbi->fix.smem_len - offset;
 	buf->num_planes = 1;
-	pipe->has_buf = 1;
 	mdss_mdp_pipe_unmap(pipe);
 
 	if (fbi->var.xres > MAX_MIXER_WIDTH || mfd->split_display) {
@@ -1906,7 +1903,6 @@ static void mdss_mdp_overlay_pan_display(struct msm_fb_data_type *mfd)
 		}
 
 		pipe->back_buf = *buf;
-		pipe->has_buf = 1;
 		mdss_mdp_pipe_unmap(pipe);
 	}
 	mutex_unlock(&mdp5_data->ov_lock);
